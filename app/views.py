3e9a17d7a5f91f4bbd56db5db8684eac3eb1a5ef
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-01-21 17:07:00 +0700

    feat: add view and urls

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
